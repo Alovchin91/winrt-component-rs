7e364b7e9de38296ee01bfda7f442710693facb5
Alexander Ovchinnikov <8490695+Alovchin91@users.noreply.github.com>
2022-01-02 00:22:21 +0100

    Don't use Result in build script

--- WinRTComponent/build.rs
+++ WinRTComponent/build.rs
@@ -2,12 +2,10 @@ use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 
-use anyhow::{Context, Result};
-
-fn main() -> Result<()> {
+fn main() {
     println!("cargo:rerun-if-changed=src/component.idl");
 
-    let crate_path : PathBuf = env::var("CARGO_MANIFEST_DIR")?.into();
+    let crate_path: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
 
     let tools_path = crate_path.parent().unwrap().join("tools");
 
@@ -22,9 +20,10 @@ fn main() -> Result<()> {
             "-DependencyVersion", "Ignore",
             "-NoCache",
             "-NonInteractive",
-            "-OutputDirectory", "packages"
+            "-OutputDirectory", "packages",
         ])
-        .status()?;
+        .status()
+        .unwrap();
 
     let sdk_dir = tools_path.join(r"packages\Microsoft.Windows.SDK.CPP.10.0.22000.196");
 
@@ -34,14 +33,10 @@ fn main() -> Result<()> {
     let foundation_contract = metadata_path.join(r"Windows.Foundation.FoundationContract.winmd");
     let universal_contract = metadata_path.join(r"Windows.Foundation.UniversalApiContract.winmd");
 
-    let cl_tool = cc::windows_registry::find_tool(&env::var("TARGET")?, "cl.exe")
-        .context("Couldn't find cl.exe compiler")?;
+    let cl_tool = cc::windows_registry::find_tool(&env::var("TARGET").unwrap(), "cl.exe").unwrap();
 
     let out_winmd = crate_path.join(r".windows\winmd\RustComponent.winmd");
-
-    std::fs::create_dir_all(
-        out_winmd.parent().unwrap()
-    ).context("Failed to create winmd directory.")?;
+    std::fs::create_dir_all(out_winmd.parent().unwrap()).unwrap();
 
     let status = Command::new(midl_path)
         .arg("/winrt")
@@ -55,7 +50,8 @@ fn main() -> Result<()> {
         .current_dir(&crate_path)
         .env("PATH", cl_tool.path().parent().unwrap())
         .env("INCLUDE", sdk_dir.join(r"c\Include\10.0.22000.0\winrt"))
-        .status()?;
+        .status()
+        .unwrap();
 
     if !status.success() {
         panic!("midl.exe command failed with exit code {}", status);
@@ -69,11 +65,7 @@ fn main() -> Result<()> {
 
     let gen_path = crate_path.join(r"src\component.rs");
 
-    std::fs::write(&gen_path, res)?;
+    std::fs::write(&gen_path, res).unwrap();
 
-    Command::new("rustfmt")
-        .arg(&gen_path)
-        .status()?;
-
-    Ok(())
+    Command::new("rustfmt").arg(&gen_path).status().unwrap();
 }
